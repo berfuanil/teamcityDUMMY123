1dce9f1115ce3d11ab5089e4e3e81d6c4e94f22a
berfuanil <berfuanil@gmail.com>
2019-05-15 20:30:39 +0300

    println modified

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!asdasd");
+        System.out.println("Hello World!asdasdasdasdadadsfsffsd  asfsaf");
     }
 }
