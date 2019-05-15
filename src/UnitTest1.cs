a41031d71fd5d2ac0325fc0f1fb4c31dd50e9774
denizbasaran <brke_dnz@windowslive.com>
2019-05-15 23:34:53 +0300

    tests

---
+++ src/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿namespace Tests
+{
+	using System;
+	using System.Configuration;
+
+	using NUnit.Framework;
+
+	[TestFixture]
+    public class UnitTest1
+    {
+		[Test]
+		[Category("Sample1")]
+		[Category("Sample2")]
+		[Category("Sample3")]
+		public void TestMethod1()
+        {
+			Console.Write("Success !!!");
+        }		
+
+		[Test]
+		[Category("Sample4")]
+		public void TestMethod2()
+		{
+			var message = ConfigurationManager.AppSettings["message"];
+			Assert.NotNull(message);
+			Console.Write(message);
+		}
+	}
+}
